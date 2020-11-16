340969adebd7842675ba0a02db1d32797866c9f5
Sidharth Arya <sidhartharya10@gmail.com>
2020-11-17 01:55:55 +0530

    Auto Commit: Tue Nov 17 01:55:55 AM IST 2020

--- Window Managers/XMonad/xmonad.hs
+++ Window Managers/XMonad/xmonad.hs
@@ -2,20 +2,30 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Util.SpawnOnce
 import System.IO
+import XMonad.Util.NamedScratchpad
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.Maximize
 
-myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
-    , className =? "Vncviewer" --> doFloat
+
+myLayout = maximizeWithPadding 0 (Tall 1 (3/100) (1/2) ||| Full)
+scratchpads = [
+    NS "Tmux" "st -c Tmux -e tmux" (className =? "Tmux") nonFloating ,
+    NS "Org" "emacs --config org" (title =? "Org") nonFloating ,
+    NS "News" "emacs --config news" (title =? "News") nonFloating ,
+    NS "Mail" "emacs --config mail" (title =? "Mail") nonFloating ,
+    NS "Tracking" "emacs --config tracking" (title =? "Tracking") nonFloating
     ]
+
+myManageHook = namedScratchpadManageHook scratchpads 
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ docks defaultConfig
+    xmonad $ docks $ ewmh defaultConfig
         { manageHook = myManageHook <+> manageHook defaultConfig -- make sure to include myManageHook definition from above
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , layoutHook = avoidStruts myLayout
 	, terminal = "st"
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
@@ -24,8 +34,15 @@ main = do
         , modMask = mod4Mask
 	, startupHook = do
 			spawnOnce "sh ~/.config/sxhkd/scripts/session_start.sh"
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
-        , ((0, xK_Print), spawn "scrot")
+        } `additionalKeysP`
+        [ ("M-S-z" , spawn "xscreensaver-command -lock")
+        , ("M-S-r" , spawn "xmonad --recompile && xmonad --restart")
+        , ("M-S-<Return>", namedScratchpadAction scratchpads "Tmux")
+        , ("M-e o", namedScratchpadAction scratchpads "Org")
+        , ("M-e n", namedScratchpadAction scratchpads "News")
+        , ("M-e t", namedScratchpadAction scratchpads "Tracking")
+        , ("M-e m", namedScratchpadAction scratchpads "Mail")
+        , ("M-x" , withFocused (sendMessage . maximizeRestore))
+        , ("C-<Print>", spawn "sleep 0.2; scrot -s")
+        , ("<Print>", spawn "scrot")
         ]
